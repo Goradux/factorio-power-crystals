2edff8aafa459ea4d9467688e2f67681ffc38649
Goradux <abbbo@mail.ru>
2023-03-14 23:07:40 +0100

    chore: moved mod data into a nested folder for easy mod loading

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
 import AdmZip from "adm-zip";
 import * as fs from "fs";
-import info from "./PowerCrystals/info.json" assert { type: "json" };
+import info from "./mod/PowerCrystals/info.json" assert { type: "json" };
 import env from "./env.json" assert { type: "json" };
 
 async function createZipArchive() {
   const zip = new AdmZip();
-  console.log(info);
   const version = info.version;
   const outputFilename = `PowerCrystals_${version}.zip`;
-  zip.addLocalFolder("./PowerCrystals");
+  zip.addLocalFolder("./mod");
   zip.writeZip(outputFilename);
   console.log(`Created ${outputFilename} successfully`);
   return outputFilename;
